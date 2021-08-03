98a30becd2e47adcfbf3619d747f9d59c552ea50
vincio <airfoil_elector0b@icloud.com>
2021-08-03 13:28:37 +0800

    added ItemsContentView

---
+++ Kahera/ItemsContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ItemsContentView.swift
+//  Kahera
+//
+//  Created by Vincio on 8/3/21.
+//
+
+import SwiftUI
+
+struct ItemsContentView: View {
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200), spacing: 10)], spacing: 34) {
+                ForEach(0..<20) { _ in
+                    ItemView()
+                }
+            }.padding(.top, 20)
+            .padding(.horizontal, 10)
+        }
+    }
+}
+
+struct ItemsContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemsContentView()
+    }
+}
