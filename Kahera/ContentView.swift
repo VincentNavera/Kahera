d35ae33b5d228ddc028ce6df668750cd4880c5b4
vincio <airfoil_elector0b@icloud.com>
2021-08-03 10:46:07 +0800

    Initial Commit

---
+++ Kahera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kahera
+//
+//  Created by Vincio on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
