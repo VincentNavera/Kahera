002888d249215a61d76993e98aa5eada606bfadd
vincio <airfoil_elector0b@icloud.com>
2021-08-05 16:21:36 +0800

    modified the itemheader

--- Kahera/Views/Items/HeaderView.swift
+++ Kahera/Views/Items/HeaderView.swift
@@ -11,40 +11,43 @@ struct HeaderView: View {
     @State private var edit = false
     @State private var title = "STORE NAME"
     var body: some View {
-        HStack {
-            Spacer()
-            HStack {
-                if !edit {
-                    Text(title)
-                        .titleFont()
-                        .onTapGesture {
-                            self.edit = true
+        ZStack {
+
+
+                HStack {
+                    if !edit {
+                        Text(title)
+                            .titleFont()
+                            .onTapGesture {
+                                self.edit = true
+                            }
+
+                    } else {
+                        HStack {
+                            TextField(title, text: $title, onCommit: {self.edit = false})
+                            Button("Done"){
+                                self.edit = false
+                            }
                         }
 
-                } else {
-                    HStack {
-                        TextField(title, text: $title, onCommit: {self.edit = false})
-                        Button("Done"){
-                            self.edit = false
-                        }
                     }
-
                 }
-            }
-            Spacer()
 
-            Button(action: { }) {
-                Image(systemName: "line.horizontal.3.decrease.circle")
-                    .font(Font.system(size: 24, weight: .thin))
-            }
-            .buttonStyle(PlainButtonStyle())
-            .foregroundColor(Color.black)
-            .padding(.trailing, 15)
+                .frame(height: 60)
+                .offset(x: -1)
 
+
+            HStack {
+                Spacer()
+                Button(action: { }) {
+                    Image(systemName: "line.horizontal.3.decrease.circle")
+                        .font(Font.system(size: 24, weight: .thin))
+                }
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(Color.black)
+                .padding(.trailing, 15)
+            }
         }
-        .frame(height: 60)
-        .background(Color.white)
-        .offset(x: -1)
     }
 }
 struct HeaderView_Previews: PreviewProvider {
