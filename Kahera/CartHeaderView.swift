fc65c2e10cd8e3db361d55ea31120eff3e977c6c
vincio <airfoil_elector0b@icloud.com>
2021-08-05 14:20:21 +0800

    added CartHeaderView

---
+++ Kahera/CartHeaderView.swift
@@ -0,0 +1,32 @@
+//
+//  CartHeaderView.swift
+//  Kahera
+//
+//  Created by Vincio on 8/5/21.
+//
+
+import SwiftUI
+
+struct CartHeaderView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("CART (0)")
+                .titleFont()
+            Spacer()
+            Image(systemName: "trash")
+                .font(Font.system(size: 24, weight: .thin))
+        }
+        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+        .frame(height: 60)
+        .background(Color.white)
+        .offset(x: -1)
+
+    }
+}
+
+struct CartHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartHeaderView()
+    }
+}
