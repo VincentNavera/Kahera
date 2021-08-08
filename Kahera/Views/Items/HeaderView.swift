318e0c44504b710467f61a34f3bc66f90b51db4e
vincio <airfoil_elector0b@icloud.com>
2021-08-08 15:51:20 +0800

    fixed header's overlapping elements when editing store name

--- Kahera/Views/Items/HeaderView.swift
+++ Kahera/Views/Items/HeaderView.swift
@@ -12,52 +12,52 @@ struct HeaderView: View {
     @State private var title = "STORE NAME"
     @State private var showAdditem = false
     var body: some View {
-        ZStack {
-                HStack {
-                    if !edit {
-                        Text(title)
-                            .titleFont()
-                            .onTapGesture {
-                                self.edit = true
-                            }
-
-                    } else {
-                        HStack {
-                            TextField(title, text: $title, onCommit: {self.edit = false})
-                            Button("Done"){
-                                self.edit = false
-                            }
+            ZStack {
+                if !edit {
+                    Text(title)
+                        .titleFont()
+                        .onTapGesture {
+                            self.edit = true
                         }
+                    HStack {
+                        Spacer()
+                        Button(action:{self.showAdditem = true}) {
+                            Image(systemName: "plus")
+                                .font(Font.system(size: 24, weight: .regular))
 
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .padding(.trailing, 15)
+
+                        Button(action: { }) {
+                            Image(systemName: "line.horizontal.3.decrease.circle")
+                                .font(Font.system(size: 24, weight: .regular))
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .padding(.trailing, 15)
                     }
-                }
 
-                .frame(height: 60)
-                .offset(x: -1)
-
-
-            HStack {
-                Spacer()
-                Button(action:{self.showAdditem = true}) {
-                    Image(systemName: "plus")
-                        .font(Font.system(size: 24, weight: .regular))
+                } else {
+                    HStack {
+                        TextField(title, text: $title, onCommit: {self.edit = false})
+                        Button("Done"){
+                            self.edit = false
+                        }
+                    }
+                    .padding(.leading, 70)
 
                 }
-                .buttonStyle(PlainButtonStyle())
-                .padding(.trailing, 15)
-
-                Button(action: { }) {
-                    Image(systemName: "line.horizontal.3.decrease.circle")
-                        .font(Font.system(size: 24, weight: .regular))
-                }
-                .buttonStyle(PlainButtonStyle())
-                .padding(.trailing, 15)
             }
-        }.foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
-        .padding()
-        .sheet(isPresented: $showAdditem, content: {
-            addItemView(showAdditem: $showAdditem)
-        })
+
+            .frame(height: 60)
+            .offset(x: -1)
+            .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+            .padding()
+            .sheet(isPresented: $showAdditem, content: {
+                addItemView(showAdditem: $showAdditem)
+            })
+
+
     }
 }
 struct HeaderView_Previews: PreviewProvider {
