e04c1cb4fd3e8588f6e30fec6d99212e7c271cf6
vincio <airfoil_elector0b@icloud.com>
2021-08-09 14:36:32 +0800

    added empty cart  function to CartHeader

--- Kahera/View/Cart/CartHeaderView.swift
+++ Kahera/View/Cart/CartHeaderView.swift
@@ -18,6 +18,10 @@ struct CartHeaderView: View {
             Image(systemName: "trash")
                 .font(Font.system(size: 24, weight: .regular))
                 .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+                .onTapGesture {
+                    cart.items = []
+                    cart.totalPrice = 0.00
+                }
         }
         .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
         .frame(height: 60)
